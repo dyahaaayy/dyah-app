6360c56a83043ac267208641cb877884a27ac502
dyahaaayy <dyahayu1659@gmail.com>
2025-05-30 19:46:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("Latihan membuat web dyahz")
-st.write(
-    "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python"
-)
+st.write("Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python")
