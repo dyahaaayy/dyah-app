3c2547ce37a6f70c3611660f553ca7a68aae42e4
dyahaaayy <dyahayu1659@gmail.com>
2025-05-30 19:51:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title("Latihan membuat web dyahz")
 st.write("Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python")
+st.image("ripe-apple-royalty-free-image-1659454396.jpg")
